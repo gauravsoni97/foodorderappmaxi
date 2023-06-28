18e1d01b1c591f6c1715f4c1659fabc1312f38d1
gaurav <gauravsoni8414@gmail.com>
2023-06-28 20:43:28 +0530

    ui ready basic

--- src/Components/Meals/MealCard.jsx
+++ src/Components/Meals/MealCard.jsx
@@ -17,8 +17,12 @@ const MealCard = ({ e }) => {
         <div class="relative">
           <input
             type="number"
-            class="block w-full p-4  text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            class="block w-36 p-4  text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="123"
+            min={1}
+            max={9}
+            step={1}
+            defaultValue={1}
           />
           <button
             type="submit"
